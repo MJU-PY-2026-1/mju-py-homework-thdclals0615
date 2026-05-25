7e6b479c6d3d5d44021dd538505497c5c7ad396b
thdclals0615 <songchimin1778@mju.ac.kr>
2026-05-25 18:29:25 +0000

    2

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ importance=["final","semi-finals","quarter-finals","round-of-16","qualifiers"]
 while True :
     print("\n=== 축구 경기 분석 및 보상 시스템 ===")
     print("1.경기데이터 입력 및 분석과 보상 분배")
-    print("2.시스템에 등록된 팀 목록 확인")   
-    print("3.시스템 종료")
+    print("2.mvp선수 선정")
+    print("3.시스템에 등록된 팀 목록 확인")   
+    print("4.시스템 종료")
     menu = int(input("메뉴를 선택하세요.:"))
     if menu == 1 :
+        print("경기데이터를 입력해주세요.")
         Ateam=team[int(input('Ateam 인덱스 번호:'))]
         Bteam=team[int(input('Bteam 인덱스 번호:'))]
         Ateam_prestige=team_prestige[int(input('Ateam 명성 인덱스 번호:'))]
@@ -44,9 +46,6 @@ while True :
 
         Ateam_goal=int(input('Ateam의 골 숫자:'))
         Bteam_goal=int(input('Bteam의 골 숫자:'))
-
-
-        
         if Ateam_goal > Bteam_goal :
             Ateam_compensation=win_compensation
             Bteam_compensation=lose_compensation
@@ -100,9 +99,36 @@ while True :
         print(f'Bteam의 보상은 {Bteam_compensation}원 입니다!!')
         pass
     elif menu == 2 :
+        print("mvp선수를 선정하겠습니다.")
+        high_score = 0
+        mvp_player = " "
+        while True :
+            print("선수의 각 포인트를 입력하세요.(주요선수들의 포인트만 입력하고 그만하려면 이름에 종료를 입력하세요)")
+            player_name = input("선수의 이름을 입력하세요 :")
+            if player_name == "종료" :
+                break
+            goals = int(input("선수의 골수 :"))
+            assists = int(input("선수의 도움수:"))
+            defence = int(input("선수의 수비 포인트:"))
+
+            total_score = 5*goals + 3*assists + 2*defence
+            print(f"{player_name} 선수의 총점: {total_score}점")
+
+            if total_score > high_score:
+                high_score = total_score
+                mvp_player = player_name
+                print(f"*** 현재까지 MVP 1순위: {mvp_player} ***")
+        print("mvp선수 선정 완료 !!!!!!")  
+        if mvp_player == " " :
+            print("선수의 정보가 없습니다.")
+        else :
+            print(f"축하합니다! mvp선수는 {mvp_player}선수입니다!")    
+
+
+    elif menu == 3 :
         print(f"등록된 팀의 목록입니다. {team=}")
         pass
-    elif menu == 3 :
+    elif menu == 4 :
         print("시스템을 종료합니다.")
         break
     else :
